b7a4fcc4fc25e1bdb7b0eecef335c3dec14b31ac
Gundmi Nagabhushana, Prasanna <Prasanna.GundmiNagabhushana@cdk.com>
2017-11-16 19:39:56 +0530

    Auth integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment= {
   production: false,
   overrideServiceHost: false,
-  edgeServerHost: 'http://localhost:8765/api/v1/',
+  edgeServerHost: 'http://172.26.116.74:8765/api/v1/',
   inventoryServiceHost: "http://fdss1ap01q.unix.ctcwest.ctc:8765/api/v1/inventory-search/",
   ticketingSignageServiceHost: "http://fdss1ap01d.unix.ctcwest.ctc:8765/api/v1/ticketing-signage/",
   defaultSearchRadius: 50,
-  authenticationServiceHost: "http://localhost:8765/api/v1/auth-server",
+  authenticationServiceHost: "http://172.26.116.74:8765/api/v1/auth-server",
   authenticationClientId: "ZmdsX21vYmlsZV9kZXZpY2U=",
   authenticationClientSecret: "n9zY8vL8WsxKQSfK47ALuRUzDA3Y5hfR",
   userName: "990012",
